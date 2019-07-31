29aff208999a0e1e299377b5166359f033c0734d
gs <marawansalahuldeen@gmail.com>
2019-07-31 12:17:28 +0200

    parsing json data

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const request = require('request');
  
 const url = "https://api.darksky.net/forecast/8d660b59c8a2423ac235e1ab537cc754/37.8267,-122.4233";
 
-request({url:url},(error,response)=>{
-        const data = JSON.parse(response.body);
-        console.log(data.currently.time);
+request({ url: url, json: true }, (error, response) => {
+        console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })
