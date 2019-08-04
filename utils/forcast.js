a0ea25dd138a0b321a868813563476c960e6aed0
gs <marawansalahuldeen@gmail.com>
2019-08-04 19:58:37 +0200

    handlig errors

---
+++ utils/forcast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+
+const forcast = (latitude,longitude,callback)=>{
+    const url = "https://api.darksky.net/forecast/8d660b59c8a2423ac235e1ab537cc754/"+latitude+","+longitude;
+    request({url:url,json:true},(error,response)=>{
+            if(error){
+                    callback("Unable to connect to the location");
+            }else if(response.body.error){
+                    callback("Error occurred");
+            }else{
+                    callback(undefined,response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.');
+            }
+    })
+}
+
+module.exports = forcast;
---
+++ utils/geocode.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const geocode = (address,callback)=>{
+    const url ="https://api.mapbox.com/geocoding/v5/mapbox.places/"+address+".json?access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g&cachebuster=1564573995890&autocomplete=true&types=region";
+
+    request({url:url,json:true},(error,response)=>{
+            if(error){
+                    callback('unable to connect to the service')
+            }else if(response.body.features.length === 0){
+                    console.log("wrong location");
+            }else{
+                    callback(undefined,{
+                            longitude:response.body.features[0].center[0],
+                            latitude:response.body.features[0].center[1],
+                            location:response.body.features[0].place_name
+                    })
+                    
+            }
+    })
+}
+
+
+module.exports = geocode
