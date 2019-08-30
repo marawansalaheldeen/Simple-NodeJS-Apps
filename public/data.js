902ccdd37a0a2802112d8740979d9257ed70d229
marawansalaheldeen <marawansalahuldeen@gmai.com>
2019-08-30 02:06:53 +0200

    pushing weather data to the front end

---
+++ public/data.js
@@ -0,0 +1,24 @@
+window.onload = ()=>{
+    const weatherform = document.querySelector('form')
+    const cityname = document.getElementById('addresssearchbutton')
+    const location = document.getElementById('address');
+
+    const messageone = document.getElementById('mone')
+    const messagetwo = document.getElementById('mtwo')
+    cityname.addEventListener('click',(event)=>{
+        const city = location.value;
+        event.preventDefault()
+        messageone.textContent = "Loading...."
+        messagetwo.textContent = ""
+    fetch('http://localhost:3000/weather?address='+city).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            messageone.textContent = data.error
+        } else {
+            messageone.textContent = data.location
+            messagetwo.textContent = data.forecast 
+        }
+    })
+})
+    })
+}
